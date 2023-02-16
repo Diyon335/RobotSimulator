6dcefc6b14896b85da6cdf1d75b1bc8ccaecba52
Diyon335 <diyonreal@gmail.com>
2023-02-16 17:17:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Autonomous Robotic Systems (2223-KEN4114)
+Assignment 2: Robot Simulator
+
+@authors:
+Diyon Wickrameratne (i6176139)
+Luca Forte (I6330944)
+Olmo Denegri (i6333396)
+Florent Didascalou (i6337071)
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
