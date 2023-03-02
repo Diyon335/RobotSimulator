3a4a38fbed314e4394fc2dff8ef43ac265c4aa77
Diyon335 <diyonreal@gmail.com>
2023-03-03 00:53:30 +0100

    added to framework

--- ea/evolutionary_algorithm.py
+++ ea/evolutionary_algorithm.py
@@ -1,17 +1,21 @@
 """
 This file initialises, allows the modification of parameters and runs the evolutionary algorithm
 """
+import random
+
 from ea.encoding import real_number_encoding
 from ea.selection import tournament_selection
 from ea.crossover_mutation import choose_parents, one_point_crossover
-from ea.selection import tournament_selection
-from ea.reproduction import reproduction_tournament_selection
+from ea.reproduction import generational_replacement
+from ea.evaluation import get_xy_phenotype, cost_rosenbrock, cost_rastrigin
 
 genotypes = {}
 
 number_of_genotypes = 5
 
 generations = 100
+
+offspring_per_generation = 5
 
 # Variables for the encoding strategy
 genotype_length = 5
@@ -29,18 +33,40 @@ tournament_k = 5
 # Variables for the crossover/mutation strategies
 
 
+"""
+IMPORTANT: 
+
+Any time you change one of these strategies/functions, be sure to check if the arguments you pass are 
+actually those that the strategy/functions require
+
+"""
+encoding_strategy = real_number_encoding
+phenotype_computer = get_xy_phenotype
+cost_function = cost_rosenbrock
+selection_strategy = tournament_selection
+reproduction_strategy = generational_replacement
+
+
 def initialise():
     """
     This function populates the genotype dictionary by using the specified encoding strategy.
-    Each genotype has a key (integer), and a fitness which is initially 1
+    Each genotype has a key (integer), a fitness which is initially 0, and its corresponding phenotype
+
+    The dictionary will be of the form:
+
+    key : value
+    int : [[list], int, int int]
+    <ID> : [genotype], fitness, tuple(phenotype_x, phenotype_y)
 
     :return: None
     """
 
-    for i in range(5):
-        genotypes[i] = real_number_encoding(genotype_length, genotype_min_range, genotype_max_range, whole_numbers), 0
+    for i in range(number_of_genotypes):
 
-    print(genotypes)
+        genotype = encoding_strategy(genotype_length, genotype_min_range, genotype_max_range, integers=whole_numbers)
+        x, y = phenotype_computer(genotype)
+
+        genotypes[i] = [genotype, 0, (x, y)]
 
 
 def run_algorithm():
@@ -53,11 +79,36 @@ def run_algorithm():
 
     initialise()
 
-    print(genotypes)
-
-    tournament_selection(genotypes, 3)
-
-    print(genotypes)
-
     for i in range(generations):
-        pass
+
+        # EVALUATION
+        # For each genotype, compute the fitness
+        for genotype_id in genotypes:
+
+            # We don't need to get phenotypes for the first generation since it's already done in initialise()
+            if i > 0:
+                # Replace old phenotype
+                genotype = genotypes[genotype_id][0]
+                genotypes[genotype_id][2] = phenotype_computer(genotype)
+
+            # Get phenotype
+            x, y = genotypes[genotype_id][2]
+
+            # Replace current fitness
+            genotypes[genotype_id][1] = cost_function(x, y)
+
+        # SELECTION
+        # For the number of desired offspring per generation, choose the best parent
+        # Holds the IDs of the parents that will be copied
+        offspring = []
+
+        for j in range(offspring_per_generation):
+
+            best_parent = selection_strategy(genotypes, k=tournament_k)
+            offspring.append(best_parent)
+
+        # REPRODUCTION
+        reproduction_strategy(genotypes, offspring)
+
+        # MUTATION / CROSS OVER
+
--- ea/reproduction.py
+++ ea/reproduction.py
@@ -2,14 +2,30 @@
 This file contains reproduction strategies
 """
 
-def reproduction_tournament_selection(genotype_dictionary, index):
 
-    '''
+def generational_replacement(genotype_dictionary, offspring):
+    """
+    Modifies the dictionary provided
+
+    Replaces all genotypes with the offspring
+
+    :param offspring: List of IDs of the offspring/best parents
     :param genotype_dictionary: The dictionary containing all genotypes
-    :param index: The index of the best parent among the k analyzed to copy
-    :return: The new dictionary after we added the copy of the best parent
-    '''
+    :return: None
+    """
 
-    genotype_dictionary[len(genotype_dictionary)] = genotype_dictionary[index]
+    # The new population size must be equal to the old population size
+    if len(offspring) != len(genotype_dictionary):
+        raise Exception("The number of new offspring does not match the number of old genotypes")
 
-    return genotype_dictionary
+    # Build a new list of offspring
+    offspring_list = []
+
+    for genotype_id in offspring:
+        offspring_list.append(genotype_dictionary[genotype_id])
+
+    genotype_dictionary.clear()
+
+    # Copy elements from offspring dictionary to genotype dictionary
+    for i, child in enumerate(offspring_list):
+        genotype_dictionary[i] = child
